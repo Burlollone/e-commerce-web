e9fe83a3ea2b68b4c646c9354397d40d0cf709a5
Burlollone <lor.man-93@hotmail.it>
2024-12-17 17:53:48 +0100

    install primevue UI

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Material from '@primevue/themes/material';
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Material
+    }
+});
 app.use(router)
 
 app.mount('#app')
