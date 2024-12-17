43d40cfdbcd2e3f10014d066d38ac2818019e34d
Burlollone <lor.man-93@hotmail.it>
2024-12-17 20:01:13 +0100

    categories menubar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,68 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Menubar } from 'primevue';
+import {ProgressSpinner} from 'primevue';
+import type { MenuItem } from 'primevue/menuitem';
 
+const loadingCategories = ref(true);
+const items = ref <MenuItem[]> ([
+    {
+        label: 'Categories',
+        items: []
+    }
+]);
+const categories = ref([]); // Stato per salvare i dati dei post
+
+onMounted(
+      fetch('https://fakestoreapi.com/products/categories')
+        .then(response => response.json())
+        .then((data : string[]) => {
+        loadingCategories.value = false;
+          data.forEach((el : string) => {
+              items.value[0].items?.push(
+                {
+                label: el
+                },
+              )
+            
+          });
+        } )
+        .catch(error =>{
+            loadingCategories.value = false;
+            console.log(error);
+        })
+  );
 
 </script>
 <template>
     <header>
         <nav>
-            <RouterLink to="/">Home</RouterLink>
+            <div class="card">
+                <Menubar :model="items" >
+                    <template #item="{ item, props, hasSubmenu, root }">
+                        <a v-ripple class="flex items-center" :class="{'no-event' : loadingCategories}" v-bind="props.action">
+                            <span>
+                                {{ item.label }}
+                            </span>
+                            <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
+                            <i v-if="loadingCategories" class="pi pi-spin pi-spinner" style="font-size: 1rem"></i>
+                            <i v-if="hasSubmenu && ! loadingCategories" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+                        </a>
+                    </template>
+                    <template #end>
+                        <div class="flex items-center gap-2">
+                            <!-- pulsanti wishlist, cart, login/logout  -->
+                        </div>
+                    </template>
+                </Menubar>
+            </div>
         </nav>
     </header>
 </template>
 
 <style>
-
-nav {
-  padding: 5px 5%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: var(--color-background-soft);
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-  z-index: 1;
+.no-event {
+    pointer-events: none;
 }
-
 
 </style>
