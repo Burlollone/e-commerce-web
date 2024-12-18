e9587184f0f24cffef882272a7c4ca50518ee053
Burlollone <lor.man-93@hotmail.it>
2024-12-18 18:44:32 +0100

    complete button template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import { Menubar } from 'primevue';
-import {ProgressSpinner} from 'primevue';
 import type { MenuItem } from 'primevue/menuitem';
+import Button from 'primevue/button';
 
 const loadingCategories = ref(true);
 const items = ref <MenuItem[]> ([
+    {
+        label: 'Home'
+    },
     {
         label: 'Categories',
         items: []
@@ -19,7 +22,7 @@ onMounted(
         .then((data : string[]) => {
         loadingCategories.value = false;
           data.forEach((el : string) => {
-              items.value[0].items?.push(
+              items.value[1].items?.push(
                 {
                 label: el
                 },
@@ -37,25 +40,29 @@ onMounted(
 <template>
     <header>
         <nav>
-            <div class="card">
-                <Menubar :model="items" >
-                    <template #item="{ item, props, hasSubmenu, root }">
-                        <a v-ripple class="flex items-center" :class="{'no-event' : loadingCategories}" v-bind="props.action">
-                            <span>
-                                {{ item.label }}
-                            </span>
-                            <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
-                            <i v-if="loadingCategories" class="pi pi-spin pi-spinner" style="font-size: 1rem"></i>
-                            <i v-if="hasSubmenu && ! loadingCategories" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
-                        </a>
-                    </template>
-                    <template #end>
-                        <div class="flex items-center gap-2">
-                            <!-- pulsanti wishlist, cart, login/logout  -->
-                        </div>
-                    </template>
-                </Menubar>
-            </div>
+            <Menubar :model="items" >
+                <template #item="{ item, props, hasSubmenu, root }">
+                    <a v-ripple class="flex items-center" :class="{'no-event' : loadingCategories}" v-bind="props.action">
+                        <span>
+                            {{ item.label }}
+                        </span>
+                        <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
+                        <i v-if="loadingCategories && item.label == 'Categories'" class="pi pi-spin pi-spinner" style="font-size: 1rem"></i>
+                        <i v-if="hasSubmenu && ! loadingCategories" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+                    </a>
+                </template>
+                <template #end>
+                    <div class="end-btn">
+                        <!--  logged = true -->
+                        <Button icon="pi pi-star" aria-label="wishlist" severity="secondary"  />
+                        <Button icon="pi pi-shopping-cart" aria-label="cart" severity="secondary"/>
+                        <Button icon="pi pi-sign-out" aria-label="logout" severity="secondary"/>
+
+                        <!-- logged = false -->
+                        <Button icon="pi pi-sign-in" aria-label="login" severity="secondary"/>
+                    </div>
+                </template>
+            </Menubar>
         </nav>
     </header>
 </template>
@@ -63,6 +70,12 @@ onMounted(
 <style>
 .no-event {
     pointer-events: none;
+}
+
+.end-btn {
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
 }
 
 </style>
